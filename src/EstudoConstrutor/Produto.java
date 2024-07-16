aa4e833b12a79df052dc739da1c9763e5366fecf
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-16 19:37:24 -0300

    creating getters

--- src/EstudoConstrutor/Produto.java
+++ src/EstudoConstrutor/Produto.java
@@ -1,8 +1,21 @@
 package EstudoConstrutor;
 
 public class Produto {
+    //Variables
     private String nome;
     private double preco;
     private int quantidade;
 
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
